d0512aeeeb5e5b60a94ff07cbc19b0cc0d3904ae
Michael <m.anderjaska@gmail.com>
2016-03-06 12:07:26 -0500

    first commit

---
+++ Reminders++(1)/GoalManager.swift
@@ -0,0 +1,13 @@
+//
+//  GoalManager.swift
+//  Reminders++
+//
+//  Created by Michael Anderjaska on 3/6/16.
+//  Copyright © 2016 Michael Anderjaska. All rights reserved.
+//
+
+import UIKit
+
+class GoalManager: NSObject {
+
+}
---
+++ Reminders++(1)/TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  Reminders++(1)
+//
+//  Created by Michael Anderjaska on 3/6/16.
+//  Copyright © 2016 Michael Anderjaska. All rights reserved.
+//
+
+import UIKit
+
+class TaskManager: NSObject {
+
+}
